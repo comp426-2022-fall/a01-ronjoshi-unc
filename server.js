83240dad43e59f355f1bb83935f0d98f7fcd3e60
ronjoshi-unc <ronjoshi@ad.unc.edu>
2022-12-08 14:44:51 -0500

    changed oprt message

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
         res.end(data);
     });
     server.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
+        console.log(`Server listening on ${port}`);
     })
 });
 // If there is an error, put it on the console error and return. 

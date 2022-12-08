01132e1de3888aa76576a32d26d6f80a4ee25b2a
ronjoshi-unc <ronjoshi@ad.unc.edu>
2022-12-08 14:45:26 -0500

    removed unnecessary comemnts

--- server.js
+++ server.js
@@ -27,28 +27,3 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
         console.log(`Server listening on port ${port}`);
     })
 });
-// If there is an error, put it on the console error and return. 
-// Do not be nice about exiting.
-
-
-
-
-
-// Define a const `server` as an arrow function using http.createServer. 
-// Use the documentation for the node.js http module. 
-// The function should have three responses: 
-// 1. status code 200, 
-// 2. set a header with content type `text/html`, and 
-// 3. end with the data that you are reading in from ./public/index.html.
-
-
-
-
-
-// Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log. 
-
-
-
-
-// That's it! You're all done!
